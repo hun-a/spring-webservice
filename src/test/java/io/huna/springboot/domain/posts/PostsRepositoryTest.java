156d155cd20a527e6868848441aa64deb9e49475
hun-a <seunghun.chan@gmail.com>
2020-02-02 00:55:43 +0900

    Create the test for my first JPA

---
+++ src/test/java/io/huna/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package io.huna.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void testSaveAndRetrieve() {
+        // given
+        String title = "test title";
+        String content = "test content";
+        String author = "huna";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
