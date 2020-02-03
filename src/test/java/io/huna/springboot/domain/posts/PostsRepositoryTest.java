d7e6d4499a9d5a12c6404b8fe4adb14c7086ec59
hun-a <seunghun.chan@gmail.com>
2020-02-04 00:22:32 +0900

    Add the feature of automatically adding created and modified time by the JPA Auditing

--- src/test/java/io/huna/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/io/huna/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,6 +1,7 @@
 package io.huna.springboot.domain.posts;
 
-import org.junit.After;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PostsRepositoryTest {
@@ -18,7 +21,7 @@ public class PostsRepositoryTest {
     @Autowired
     PostsRepository postsRepository;
 
-    @After
+    @AfterEach
     public void cleanup() {
         postsRepository.deleteAll();
     }
@@ -43,5 +46,27 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void testRegisterBaseTimeEntity() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019, 12, 31, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        log.info(">>>>>> createdDate={}, modifiedDate={}", posts.getCreatedDate(), posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
